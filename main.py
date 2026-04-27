94718ec997a997533c81f0d7220dd5d161f156f2
naubass <naufalnajmi04@gmail.com>
2026-04-27 13:48:13 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load Model dan Features
+model = pickle.load(open('model_rf.pkl', 'rb'))
+features = pickle.load(open('features.pkl', 'rb'))
+
+class UserInput(BaseModel):
+    sleep_hours: float
+    stress_level: float
+    daily_social_media_hours: float
+    anxiety_level: float
+    gender: int
+    platform_usage: int
+    social_interaction_level: int
+    academic_performance: int
+    screen_time_before_sleep: int
+    physical_activity: int
+    age: int
+    addiction_level: int
+
+# Endpoint API untuk Prediksi
+@app.post("/predict")
+def predict(data: UserInput):
+    df = pd.DataFrame([data.dict()])
+    df = df[features]
+    
+    prediction = model.predict(df)[0]
+    probability = model.predict_proba(df)[0][1]
+    
+    return {
+        "prediction": "Depression" if prediction == 1 else "No Depression",
+        "confidence": round(float(probability) * 100, 2)
+    }
+
+# Melayani File HTML (Frontend)
+@app.get("/")
+def index():
+    return FileResponse("index.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
